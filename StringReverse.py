1f85f00363cf2c740bc014481dfc5c1ee7452fe5
0x90Sleds <30665797+0x90Sleds@users.noreply.github.com>
2017-08-15 12:17:27 -0400

    Create StringReverse.py

---
+++ StringReverse.py
@@ -0,0 +1,5 @@
+def StringReverse():
+    string = input ("Enter a string to reverse here \n")
+    print (string[::-1])
+                    #Why do I have to do this? This is literally four lines of code.
+StringReverse()
